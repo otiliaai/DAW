e1bda8b28c134fb9ab7687db48fa615cc09c695f
Otilia <ionelotilia.r@gmail.com>
2025-12-26 18:45:31 +0000

    Adaugat folderul Laborator2

---
+++ Laborator2/Paritate.cs
@@ -0,0 +1,19 @@
+﻿class Paritate { 
+    static void Main() {
+        int n = int.Parse(Console.ReadLine());
+        int[] v = new int[n];
+        bool ok = true;
+        for (int i = 0; i < n; i++) {
+            v[i] = int.Parse(Console.ReadLine());
+        }
+        for (int i = 0; i < n-1; i++) {
+            if (v[i]%2 == v[i+1]%2) {
+                Console.WriteLine("Doua elemente alaturate cu aceeasi paritate");
+                ok = false;
+            } 
+        }
+        if (ok) {
+            Console.WriteLine("Nu exista doua elemente alaturate cu aceeasi paritate");
+        }
+    }
+};
---
+++ Laborator2/Program.cs
@@ -0,0 +1,54 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string nr1,nr2 = "";
+        Console.WriteLine("Introduceti un numar: ");
+        nr1 = Console.ReadLine();
+        for (int i = nr1.Length - 1; i >= 0; i--)
+        {
+            nr2 += nr1[i];
+        }
+
+        if (nr1 == nr2)
+        {
+            Console.WriteLine("Palindrom");
+        }
+        else
+        {
+            Console.WriteLine("Nu este palindrom");
+        }
+
+    }
+}
+
+///ALTERNATIV CU METODA
+// class Program
+// {
+//     void Palindrom (int n)
+//     {
+//         string nr = n.ToString();
+//         string nr2 = "";
+//         for (int i = nr.Length - 1; i >= 0; i--)
+//         {
+//             nr2 += nr[i];
+//         }
+//         if (nr == nr2)
+//         {
+//             Console.WriteLine("Palindrom");
+//         }
+//         else
+//         {
+//             Console.WriteLine("Nu este palindrom");
+//         }
+//     }
+//     static void Main(string[] args)
+//     {
+//         Console.WriteLine("Introduceti un numar: ");
+//         int n = Convert.ToInt32(Console.ReadLine());
+//         Program p = new Program();
+//         p.Palindrom(n);
+
+
+//     }
+// }
